56010118cde6ec0ecda3930fb05bdbf4274d1064
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-10 11:08:57 -0400

    Have to different banners displaying one for Rent a home and the other for Buy a Home

--- realestate/pages/index.js
+++ realestate/pages/index.js
@@ -25,7 +25,10 @@ const Home = () => {
       <Banner purpose="Rent a Home" title1="Rental Homes for" title2="Everyone" desc1="Explore Homes and Apartments"
         desc2="and more" buttonText="Explore Renting" linkName="/search?purpose=for-rent" imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <Banner purpose={'For Rent'} />
+      <Banner purpose="Buy a Home" title1="Find, Buy and Own Your" title2="Dream Home" desc1="Explore Homes and Apartments"
+        desc2="and more" buttonText="Explore Buying" linkName="/search?purpose=for-sale" imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+
+      />
     </div>
   )
 }
