52e57211297eb7397a243487af7be49de30d12b5
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-13 11:09:49 -0400

    Importing packages from next, chakra-ui, react-icons and millify and i quickly added the property title, price and image

--- realestate/components/property/Property.js
+++ realestate/components/property/Property.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import { Box, Flex, Text, Avatar } from '@chakra-ui/react'
+import { FaBed, FaBath } from 'react-icons/fa'
+import { BsGridFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
+import millify from "millify"
 
 const Property = ({ property }) => {
+  console.log(property)
   return (
-    <div>Property</div>
+    <>
+      <div>
+        <h2>Title: {property.title}</h2>
+        <br />
+        <p>Price: ${millify(property.price)}</p>
+        <img src={property.coverPhoto.url} alt='home img' />
+      </div>
+    </>
   )
 }
 
