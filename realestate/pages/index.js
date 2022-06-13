938ad1bfeb4e24a245cf8b8e652e70499d1ef48b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-13 10:34:30 -0400

    Created a get request to fetch all of the data

--- realestate/pages/index.js
+++ realestate/pages/index.js
@@ -11,24 +11,27 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, imageUrl, l
       <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
       <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
       <Text color="gray.700" fontSize="lg" paddingTop="3px" paddingBottom="3px" fontWeight="medium">{desc1}<br />{desc2}</Text>
-      <Button fontSize="xl" bg="blue.300" color="white">
+      <Button fontSize="xl">
         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
 )
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <Box>
       <Banner purpose="Rent a Home" title1="Rental Homes for" title2="Everyone" desc1="Explore Homes and Apartments"
         desc2="and more" buttonText="Explore Renting" linkName="/search?purpose=for-rent" imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
+      <Flex flexWrap="wrap">
+        {/* Fetch properties and map over them */}
+      </Flex>
       <Banner purpose="Buy a Home" title1="Find, Buy and Own Your" title2="Dream Home" desc1="Explore Homes and Apartments"
         desc2="and more" buttonText="Explore Buying" linkName="/search?purpose=for-sale" imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-
       />
-    </div>
+      {/* Fetch properties and map over them */}
+    </Box>
   )
 }
 
---
+++ realestate/utils/fetchapi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseURL = 'https://bayut.p.rapidapi.com'
+
+export const FetchApi = async (url) => {
+  const { data } = await axios.get((url), {
+    headers: {
+      'X-RapidAPI-Key': '655bdaa00dmsh1dfed893f79b687p102403jsn162126e9f66e',
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+    }
+  })
+}
