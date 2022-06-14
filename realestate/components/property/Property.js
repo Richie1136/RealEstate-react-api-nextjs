2cda1dd458d7b63948d0a722837c0271e2bff064
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-14 11:38:11 -0400

    Added a default image and Using chakra-ui to style my Property component

--- realestate/components/property/Property.js
+++ realestate/components/property/Property.js
@@ -6,9 +6,15 @@ import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
 import millify from "millify"
 
+let defaultImage = 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bHV4dXJ5JTIwaG91c2V8ZW58MHx8MHx8&w=1000&q=80'
+
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID } }) => (
-  <Link href={`/property/${externalID}`} passHref >
-    <div>
+  // p stands for padding, w stands for width
+  <Link href={`/property/${externalID}`} passHref>
+    <Flex flexWrap="wrap" w="420px" p="5px" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+      <Box>
+        <Image src={coverPhoto ? coverPhoto.url : defaultImage} alt='house' width={500} height={500} />
+      </Box>
       <h2>Title: {title}</h2>
       <h4>Rooms: {rooms}</h4>
       <h4>Baths: {baths}</h4>
@@ -18,8 +24,7 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
       <h6>Rent Frequency: {rentFrequency}</h6>
       <br />
       <p>Price: ${millify(price)}</p>
-      <img src={coverPhoto.url} alt='home img' />
-    </div>
+    </Flex>
   </Link>
 )
 
