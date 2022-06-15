12bf83f4d54dee348f2c21e21a87756767c1227f
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-15 11:13:10 -0400

    Created a footer and navbar and imported it inside of my layout component

---
+++ realestate/components/layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from '../footer/Footer.js'
+import NavBar from '../navbar/NavBar.js'
+
+const Layout = () => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
