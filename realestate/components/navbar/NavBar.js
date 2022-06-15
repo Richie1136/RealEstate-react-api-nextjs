be51b597ac661c90377c8e4ed046c628378dae92
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-15 11:44:54 -0400

    Importing packages from chakra-ui

--- realestate/components/navbar/NavBar.js
+++ realestate/components/navbar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react'
 
 const NavBar = () => {
   return (
