0e86046404edf10ed59246901cdd857d147fb9fa
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-15 11:19:10 -0400

    Adding the layout inside of the layout component

--- realestate/components/layout/Layout.js
+++ realestate/components/layout/Layout.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Footer from '../footer/Footer.js'
 import NavBar from '../navbar/NavBar.js'
+import Head from 'next/head.js'
+import { Box } from '@chakra-ui/react'
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <>
+      <Head>
+        <title>Real Estate App</title>
+      </Head>
+      <Box maxWidth="1280px" m="auto">
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          {children}
+        </main>
+      </Box>
+      <Footer />
+    </>
   )
 }
 
--- realestate/components/navbar/NavBar.js
+++ realestate/components/navbar/NavBar.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header>
+      <div>Real Estate App</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>All Meetups</Link>
+          </li>
+          <li>
+            <Link href='/newmeetup'>Add New Meetup</Link>
+          </li>
+        </ul>
+      </nav>
+
+    </header>
   )
 }
 
