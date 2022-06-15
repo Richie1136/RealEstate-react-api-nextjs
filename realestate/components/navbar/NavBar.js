5f3b4b571d4bdbd66b1a220ba4823893b176d7e3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-15 11:57:00 -0400

    Building the navBar component

--- realestate/components/navbar/NavBar.js
+++ realestate/components/navbar/NavBar.js
@@ -2,24 +2,35 @@ import React from 'react'
 import Link from 'next/link'
 import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react'
 import { FcMenu, FcHome, FcAbout } from 'react-icons/fc'
+import { BsSearch } from 'react-icons/bs'
+import { FiKey } from 'react-icons/fi'
 
-const NavBar = () => {
-  return (
-    <header>
-      <div>Real Estate App</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/'>All Properties</Link>
-          </li>
-          <li>
-            <Link href='/newmeetup'>Property Data</Link>
-          </li>
-        </ul>
-      </nav>
+const NavBar = () => (
+  <Flex p="2" borderBottom="1px" borderColor="gray.100">
+    <Box fontSize="3xl" color="blue.400" fontWeight="bold">
+      <Link href='/' paddingLeft="2px">Real Estate Application</Link>
+    </Box>
+    <Spacer />
+    <Box>
+      <Menu>
+        <MenuButton color="red.400" as={IconButton} icon={<FcMenu />} variant="outlined" />
+        <MenuList>
+          <Link href='/' passHref>
+            <MenuItem icon={<FcHome />}>Home</MenuItem>
+          </Link>
+          <Link href='/' passHref>
 
-    </header>
-  )
-}
+          </Link>
+          <Link href='/' passHref>
+
+          </Link>
+          <Link href='/' passHref>
+
+          </Link>
+        </MenuList>
+      </Menu>
+    </Box>
+  </Flex>
+)
 
 export default NavBar
