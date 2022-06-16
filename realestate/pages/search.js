4235729d898abefac9204d3e77405c411e890fbd
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-16 11:24:54 -0400

    Mapping over all of the properties and if the length is 0 then im going to display a no results to show image

--- realestate/pages/search.js
+++ realestate/pages/search.js
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react'
 import { BsFilter } from 'react-icons/bs'
 import SearchFilters from '../components/searchfilters/SearchFilters'
+import Property from '../components/property/Property'
+import noresult from '../assets/noresult.jpg'
 
 const Search = () => {
   const [searchFilters, setSearchFilters] = useState(false)
@@ -31,6 +33,17 @@ const Search = () => {
         <Icon paddingLeft="2" w="7" as={BsFilter}></Icon>
       </Flex>
       {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" padding='4' fontWeight='bold'>
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap="wrap">
+        {[].map((property) => <Property key={property.id} property={property} />)}
+      </Flex>
+      {[].length === 0 && (
+        <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
+          <Image src={noresult} alt='No Result' />
+        </Flex>
+      )}
     </Box>
   )
 }
