33e0654f233e8acf041b2a37d0dddffe4d668d5e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-16 11:03:38 -0400

    Started building out the search page

--- realestate/pages/search.js
+++ realestate/pages/search.js
@@ -5,8 +5,26 @@ import { Flex, Box, Text, Icon } from '@chakra-ui/react'
 import { BsFilter } from 'react-icons/bs'
 
 const Search = () => {
+  const [searchFilters, setSearchFilters] = useState(false)
+
+  const router = useRouter()
   return (
-    <div>Search</div>
+    <Box>
+      <Flex
+        cursor="pointer"
+        bg="gray.100"
+        borderBottom="1px"
+        borderColor="gray.200"
+        p="2"
+        fontWeight="black"
+        fontSize="large"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>Search Property By Filters</Text>
+        <Icon paddingLeft="2" w="7" as={BsFilter}></Icon>
+      </Flex>
+    </Box>
   )
 }
 
