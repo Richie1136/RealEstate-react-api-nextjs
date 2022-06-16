2e56bcc503899db713815a5f66e0594d0a2806d6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-16 11:08:14 -0400

    Wrote the functionality to toggle the filter and then under the flex component if i have search filters then i want to display the search filters component which i am going to create now

--- realestate/pages/search.js
+++ realestate/pages/search.js
@@ -3,9 +3,14 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react'
 import { BsFilter } from 'react-icons/bs'
+import SearchFilters from '../components/searchfilters/SearchFilters'
 
 const Search = () => {
   const [searchFilters, setSearchFilters] = useState(false)
+
+  const handleClick = () => {
+    setSearchFilters(prevFilter => !prevFilter)
+  }
 
   const router = useRouter()
   return (
@@ -20,10 +25,12 @@ const Search = () => {
         fontSize="large"
         justifyContent="center"
         alignItems="center"
+        onClick={handleClick}
       >
         <Text>Search Property By Filters</Text>
         <Icon paddingLeft="2" w="7" as={BsFilter}></Icon>
       </Flex>
+      {searchFilters && <SearchFilters />}
     </Box>
   )
 }
