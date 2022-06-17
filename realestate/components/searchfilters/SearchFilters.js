ca0575dbe631b3b674648706afbf864146906aa8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-17 10:43:47 -0400

    Created a filterData file which has all of the different filters and then imported that file inside of my searchFilters component

--- realestate/components/searchfilters/SearchFilters.js
+++ realestate/components/searchfilters/SearchFilters.js
@@ -3,10 +3,14 @@ import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-u
 import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
+import { filterData } from '../../utils/filterData'
 
 const SearchFilters = () => {
+  const [filters, setFilters] = useState({})
   return (
-    <div>SearchFilters</div>
+    <Flex bg='gray.100' p="4" justifyContent="center" flexWrap='wrap'>
+
+    </Flex>
   )
 }
 
