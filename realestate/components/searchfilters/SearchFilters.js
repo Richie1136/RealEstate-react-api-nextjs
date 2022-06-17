561f513fd2c9a0aaa5fae0edcf4b80bd177f37af
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-17 11:09:18 -0400

    Have the different filters displaying

--- realestate/components/searchfilters/SearchFilters.js
+++ realestate/components/searchfilters/SearchFilters.js
@@ -3,14 +3,31 @@ import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-u
 import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
-import { filterData } from '../../utils/filterData'
+import { filterData, getFilterValues } from '../../utils/filterData'
 
 const SearchFilters = () => {
-  const [filters, setFilters] = useState({})
+  const [filters, setFilters] = useState(filterData)
+  console.log(filterData)
+
+
+  const searchProperties = (filterValues) => {
+
+  }
+
+
+  const handleChange = (e, queryName) => {
+    searchProperties({ [filter.queryName]: e.target.value })
+  }
+
   return (
     <Flex bg='gray.100' p="4" justifyContent="center" flexWrap='wrap'>
-
-    </Flex>
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select p='2' w='fit-content' placeholder={filter.placeholder} onChange={handleChange}>
+          </Select>
+        </Box>
+      ))}
+    </Flex >
   )
 }
 
