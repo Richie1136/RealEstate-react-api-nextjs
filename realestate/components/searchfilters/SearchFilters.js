85005b10ae0a5fdb31747a781d28082f28695598
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-20 10:59:25 -0400

    Deleted function that was causing an error and moved that code into the onChange on my Select component

--- realestate/components/searchfilters/SearchFilters.js
+++ realestate/components/searchfilters/SearchFilters.js
@@ -6,24 +6,31 @@ import Image from 'next/image'
 import { filterData, getFilterValues } from '../../utils/filterData'
 
 
+
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData)
+  const router = useRouter()
   console.log(filterData)
 
 
   const searchProperties = (filterValues) => {
-  }
+    const path = router.pathname
+    const { query } = router
 
+    const values = getFilterValues(filterValues)
 
-  const handleChange = (e, queryName) => {
-    searchProperties({ [filter.queryName]: e.target.value })
+    values.map((item) => {
+      query[item.name] = item.value
+    })
+
+    router.push({ pathname: path, query })
   }
 
   return (
     <Flex bg='gray.100' p="4" justifyContent="center" flexWrap='wrap'>
       {filters?.map((filter) => (
         <Box key={filter.queryName}>
-          <Select p='2' w='fit-content' placeholder={filter.placeholder} onChange={handleChange}>
+          <Select p='2' w='fit-content' placeholder={filter.placeholder} onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}>
             {filter?.items?.map((item) => (
               <option value={item.value} key={item.value}>{item.name}</option>
             ))}
